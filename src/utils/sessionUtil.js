9bfcf3a4d4978eb7c79109504bb40c8ae6afbb9d
HarishPakki <harishpakki@gmail.com>
2025-07-06 13:41:33 +0530

    tree

--- src/utils/sessionUtil.js
+++ src/utils/sessionUtil.js
@@ -1,12 +1,12 @@
 export const saveToSession = (key, value) => {
-  sessionStorage.setItem(key, JSON.stringify(value));
+    sessionStorage.setItem(key, JSON.stringify(value));
 };
 
 export const getFromSession = (key) => {
-  const val = sessionStorage.getItem(key);
-  return val ? JSON.parse(val) : null;
+    const val = sessionStorage.getItem(key);
+    return val ? JSON.parse(val) : null;
 };
 
 export const clearSession = () => {
-  sessionStorage.clear();
+    sessionStorage.clear();
 };
